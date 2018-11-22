4b7dbb7116ce0f9e64714a03c9f470c8b6d6fcf5
nnqvinh112 <nnqvinh112@gmail.com>
2018-11-22 14:46:41 +0700

    Get store data from server

--- FrontEnd/src/app/shared/models/store.ts
+++ FrontEnd/src/app/shared/models/store.ts
@@ -1,18 +1,25 @@
 import { Base } from './base';
+import { Employee } from './employee';
 
 export class Store extends Base {
   'id': number;
   'name': string;
   'address': string;
   'logo': string;
-  'owner': string;
+  'ownerName': string;
+  'ownerId': number;
+  'employees': Employee[];
 
   constructor(item) {
-    super(item.createdBy, item.createdDate, item.updatedBy, item.updatedDate);
-    this.id = item.id;
+    super(item.createdBy, item.createDay, item.updatedBy, item.updatedDate);
+    this.id = item.id || item['idCuahang'];
     this.name = item.name;
     this.address = item.address;
-    this.logo = item.logo;
-    this.owner = item.owner;
+    this.logo = item.logo || 'https://www.freeiconspng.com/uploads/no-image-icon-4.png';
+    this.ownerName = item.owner;
+    this.employees = [];
+    if (item['nhanviensByIdCuahang']) {
+      this.employees = item['nhanviensByIdCuahang'].map(i => new Employee(i));
+    }
   }
 }
