8721d20aaf4a53f4abdb94d5f03c3629e90585c5
DESKTOP-9927S41\Admin <nddnam2503@gmail.com>
2018-11-26 23:23:55 +0700

    no message

--- FrontEnd/src/app/provider/category.service.ts
+++ FrontEnd/src/app/provider/category.service.ts
@@ -3,25 +3,33 @@ import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { Category } from '../shared/models';
+import { API } from '../shared/constants';
 
-const API_URL = './../../assets/data-json/list-category.json';
 @Injectable()
 export class CategoryService {
   constructor(private httpClient: HttpClient) { }
 
   getAll() {
-    return this.httpClient.get(`${API_URL}`)
+    return this.httpClient.get(`${API.ROOT}/category`)
       .pipe(
-        map((body: any[]) => {
-          body.forEach((value, index, array) => {
-            array[index] = new Category(value);
-            // console.log(value);
-            // console.log(array[index]);
-          });
-          return body;
-        },
-            catchError(() => of('Error, could not load joke :-(')),
+        map(
+          (body: any) => body['data'].map(i => new Category(i)),
+          catchError(() => of('Error, could not load category from server')),
         ),
+      );
+  }
+
+  add(category) {
+    return this.httpClient.post(`${API.ROOT}/category/create/`, category)
+      .pipe(
+          catchError(() => of('Error, could not add category')),
+      );
+  }
+
+  update(id, newNameCategory) {
+    return this.httpClient.put(`${API.ROOT}/category/${id}`, newNameCategory)
+      .pipe(
+          catchError(() => of('Error, could not update category')),
       );
   }
 }
